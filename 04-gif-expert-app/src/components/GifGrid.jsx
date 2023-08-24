cb545737f23757c3e29e3c9ba88f0e66ce4dc59f
cmayen <carmelo5mayen@gmail.com>
2023-08-24 15:43:01 -0600

    useEffect

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
 
-        getGifs( category );
+    const [counter, setCounter] = useState(10);
+    
+    useEffect( () => {
+      //first
+      getGifs( category );     
+    }, [/*third*/])
+    
+
 
     return (
     <>
         <h3>{ category }</h3>
+
+        <h5>{ counter }</h5>
+        <button onClick={ () => setCounter(counter +1) }>+1</button>
         
     </>
   )
--- 04-gif-expert-app/src/helpers/getGifs.js
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async ( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=GYELZ4aPPKr1sXB0y3brVE3ARedw8YXa&q=${ category }&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=GYELZ4aPPKr1sXB0y3brVE3ARedw8YXa&q=${ category }&limit=10`;
     const resp = await fetch( url );
 
     const { data } = await resp.json();
